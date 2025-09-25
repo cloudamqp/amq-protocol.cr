4bb126f11105a5b510c9635251bf36cc16f08926
Anders Bälter <anders@84codes.com>
2025-09-25 23:41:00 +0200

    Fix Exchange::Bind frame bytesize calculation
    The previous calculation incorrectly included an extra 2 bytes (2 + 2 + 1 + ...)
    when it should only include the reserved1 field (2 bytes) once (2 + 1 + ...).
    
    This was causing frame size miscalculations that could lead to protocol errors
    when binding exchanges. The fix removes the duplicate 2-byte addition.
    
    Added test case to verify the correct bytesize calculation.

--- spec/frame_spec.cr
+++ spec/frame_spec.cr
@@ -17,4 +17,17 @@ describe AMQ::Protocol::Frame::Method::Basic::Get do
     frame.to_io(io, IO::ByteFormat::SystemEndian)
     frame.to_slice.should eq io.to_slice
   end
+
+  it "can encode Exchange::Bind frame with correct bytesize" do
+    destination = "my_destination"
+    source = "my_source"
+    routing_key = "my_routing_key"
+    arguments = AMQ::Protocol::Table.new
+    frame = AMQ::Protocol::Frame::Exchange::Bind.new(1u16, 0u16, destination, source, routing_key, false, arguments)
+    # Method body size: reserved1 + destination + source + routing_key + no_wait + arguments
+    method_body_size = 2 + 1 + destination.bytesize + 1 + source.bytesize + 1 + routing_key.bytesize + 1 + arguments.bytesize
+    # Total frame size includes 4 bytes for class_id + method_id
+    expected_bytesize = method_body_size + 4
+    frame.bytesize.should eq expected_bytesize
+  end
 end
